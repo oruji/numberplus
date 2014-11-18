fdae7b315a23e213ddcdf2bf338c541c429cbcd0
Amin Oruji <amin@friend.ir>
2014-11-18 10:09:03 +0330

    add to persian method

--- src/main/java/numberplus/NumberPlus.java
+++ src/main/java/numberplus/NumberPlus.java
@@ -1,7 +1,21 @@
 package numberplus;
 
 public class NumberPlus {
+	private static final int convertNumber = 1728;
+
 	public static void main(String[] args) {
-		System.out.println("Hello Number Plus");
+		System.out.println(toEnglish("3۳۲۲۳sdf234"));
+	}
+
+	public static String toEnglish(String number) {
+		char[] charArr = number.toCharArray();
+
+		for (int i = 0; i < charArr.length; i++) {
+			if (charArr[i] >= 1776 && charArr[i] <= 1785) {
+				charArr[i] = (char) (charArr[i] - convertNumber);
+			}
+		}
+
+		return new String(charArr);
 	}
 }

960a6d2f064ae66c2f7e6c267331aff7249c9572
Amin Oruji <amin@friend.ir>
2014-11-18 10:44:33 +0330

    add toEnglish method

---
+++ src/main/java/numberplus/Main.java
@@ -0,0 +1,8 @@
+package numberplus;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new NumberPlus("۳۲۴.۲۳۴").toEnglish());
+		System.out.println(new NumberPlus("2342.2342").toPersian());
+	}
+}
--- src/main/java/numberplus/NumberPlus.java
+++ src/main/java/numberplus/NumberPlus.java
@@ -1,18 +1,40 @@
 package numberplus;
 
-public class NumberPlus {
+public final class NumberPlus {
 	private static final int convertNumber = 1728;
 
-	public static void main(String[] args) {
-		System.out.println(toEnglish("3۳۲۲۳sdf234"));
+	private final char[] value;
+
+	private NumberPlus() {
+		this.value = new char[0];
 	}
 
-	public static String toEnglish(String number) {
-		char[] charArr = number.toCharArray();
+	public NumberPlus(String value) {
+		this.value = value.toCharArray();
+	}
+
+	public NumberPlus(Double value) {
+		this.value = value.toString().toCharArray();
+	}
+
+	public String toEnglish() {
+		char[] charArr = this.value;
 
 		for (int i = 0; i < charArr.length; i++) {
 			if (charArr[i] >= 1776 && charArr[i] <= 1785) {
 				charArr[i] = (char) (charArr[i] - convertNumber);
+			}
+		}
+
+		return new String(charArr);
+	}
+
+	public String toPersian() {
+		char[] charArr = this.value;
+
+		for (int i = 0; i < charArr.length; i++) {
+			if (charArr[i] >= 48 && charArr[i] <= 57) {
+				charArr[i] = (char) (charArr[i] + convertNumber);
 			}
 		}
 
